d97533d58350b48d7e56d5938c3c0cee0921d3cf
mac <tobiak2011@gmail.com>
2021-05-24 14:58:07 +0100

    code refactoring

---
+++ Pods/Target Support Files/Pods-GithubCommitsAPI-GithubCommitsAPIUITests/Pods-GithubCommitsAPI-GithubCommitsAPIUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_GithubCommitsAPI_GithubCommitsAPIUITests : NSObject
+@end
+@implementation PodsDummy_Pods_GithubCommitsAPI_GithubCommitsAPIUITests
+@end
---
+++ Pods/Target Support Files/Pods-GithubCommitsAPI/Pods-GithubCommitsAPI-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_GithubCommitsAPI : NSObject
+@end
+@implementation PodsDummy_Pods_GithubCommitsAPI
+@end
